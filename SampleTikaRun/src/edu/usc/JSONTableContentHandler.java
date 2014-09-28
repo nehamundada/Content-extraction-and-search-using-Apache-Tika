032be508bde033021296c064f845588d7cd6dd54
Saurabh Chaure <chaure@usc.edu>
2014-09-27 21:07:08 -0700

    Json Object

--- SampleTikaRun/src/edu/usc/JSONTableContentHandler.java
+++ SampleTikaRun/src/edu/usc/JSONTableContentHandler.java
@@ -5,42 +5,44 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.apache.tika.sax.ContentHandlerDecorator;
+import org.json.JSONObject;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 
 public class JSONTableContentHandler extends ContentHandlerDecorator {
 
-	StringBuffer buffer = null ;
-	final List <String> skipList = new ArrayList<String>( Arrays.asList("html", "head", "meta", "title", "body"));
+	final List <String> skipList = new ArrayList<String>( Arrays.asList("html", "head", "meta", "title", "body") );
+	String elementName = "";
 
+	JSONObject json = null;
+	
 	public JSONTableContentHandler() {
 	}
 
 	public void startDocument() throws SAXException {
-		buffer = new StringBuffer();
-		buffer.append("{");
+		  json = new JSONObject();
 	}
 
 	public void endDocument() throws SAXException {
-		buffer.append("}");
- 	}
-	public void characters(char[] ch, int start, int length)
-			throws SAXException {
-		if(ch.length > 0 && !skipList.contains(ch))
-			buffer.append( "\""+ new String(ch) +"\" " );
+	}
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		if(ch.length > 0 && !skipList.contains(ch)){
+			try{
+				json.put(elementName, new String(ch));
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
 	}
 
-	public void startElement(String uri, String localName, String name,
-			Attributes atts) throws SAXException {
-		if(!skipList.contains(name))
-			buffer.append("\""+name+"\" : ");
+	public void startElement(String uri, String localName, String name, Attributes atts) throws SAXException {
+		if(!skipList.contains(name)){
+			elementName = name;
+		}
 	}
- 	public void endElement(String uri, String localName, String name)
-			throws SAXException {
-		if(!skipList.contains(name))
-			buffer.append(", ");
-	}
-  	public String toString() {
- 		return buffer.toString();
+	public void endElement(String uri, String localName, String name) throws SAXException {
+ 	}
+	public String toString() {
+		return json.toString();
 	}
 } 

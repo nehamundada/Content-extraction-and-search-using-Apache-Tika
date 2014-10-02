a97419b41103b9f84be3cbf0633ed9bf18c03f90
Saurabh Chaure <chaure@usc.edu>
2014-10-02 11:53:39 -0700

    changes to handler

--- SampleTikaRun/src/edu/usc/FileCounter.java
+++ SampleTikaRun/src/edu/usc/FileCounter.java
@@ -1,22 +1,35 @@
 package edu.usc;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.Map;
 
 public class FileCounter {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		int totalCount = 0;
-		File f = new File("F:/COURSES/Sem3/CSCI_572_Information Retreival and Search Engines/Assignment1/JSON");
+		File f = new File("F:/COURSES/Sem3/CSCI_572_Information Retreival and Search Engines/Assignment1/JSONDATA");
 		File files [] = f.listFiles();
-		
-		int ctr = 1;
+ 		
+		int ctr = 0;
+		String line = null;
 		for (File file : files) {
-			int count = file.listFiles().length;
-			totalCount = totalCount + count;
-			System.out.println( ctr + "	, 	"+ file.getName()+ " 	, Count , " + count );
-			ctr ++ ;
-		}
-		System.out.println(" ************ TOTAL : " + totalCount);
+			File countfile = new File(file+"/count.txt");
+			BufferedReader reader = new BufferedReader( new InputStreamReader(new FileInputStream(countfile)));
+			while(( line=reader.readLine() )!=null){
+				int count = Integer.parseInt(line);
+				totalCount = totalCount + count;
+				System.out.println(file + " : " + count);
+			}
+			reader.close();
+			ctr++;
+ 		}
+		System.out.println(" ****** TOTAL FILES ****** " + ctr);
+		System.out.println(" ************ TOTAL COUNT ********** : " + totalCount);
+		
 	}
 }

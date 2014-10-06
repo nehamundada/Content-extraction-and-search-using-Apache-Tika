a12badd01913f9cc6d9c8a1e7983141684be3ccc
Shrikanth Narayanan <nara471@usc.edu>
2014-10-05 18:58:30 -0700

    updated run script

--- SampleTikaRun/run.sh
+++ SampleTikaRun/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+rm -r bin/*
+
 javac -cp ./src:lib/opencsv-3.0.jar:lib/tika-app-1.6.jar:lib/java-json.jar:lib/simmetrics_jar_v1_6_2_d07_02_07.jar:lib/ -d bin/ src/edu/usc/Main.java
 
 java -cp ./bin:lib/opencsv-3.0.jar:lib/tika-app-1.6.jar:lib/java-json.jar:lib/simmetrics_jar_v1_6_2_d07_02_07.jar:lib/ edu.usc.Main
